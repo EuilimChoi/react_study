370f015042990e3def189dd18d6e8d0a8ba17bd5
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-02-04 22:01:45 +0900

    디테일 페이지 작성

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,40 +1,22 @@
-import { useEffect, useState } from "react";
-import Movie from './component/Movie';
-
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Detail from "./router/Detail";
+import Home from './router/Home';
 
 function App() {
-  // const [loading,setloding] = useState(true)
-  // const [movies,setmovies] = useState([])
-
-
-  // const getMovies = async() => {
-  //   const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
-  //   const moviedata = await response.json();
-  //   setmovies(moviedata.data.movies)
-  //   setloding(false)
-  // }
-
-  // useEffect(() => {getMovies();},[])
-
-  // console.log(movies)
-
-  // return (
-  //   <div>
-  //     {loading ? <h1>Loding....</h1> : movies.map((el) => (
-
-  //     <Movie
-  //     key = {el.title}
-  //     img={el.medium_cover_image} 
-  //     title={el.title} 
-  //     rating={el.rating} 
-  //     genres={el.genres} 
-  //     summary={el.summary}/>
-
-  //     ))}
-  //   </div>
-  // )
-  return null
+  return <Router>
+            <Switch>
+              <Route path="/hello">
+              <h1>Hello world!</h1>
+              </Route>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/movie/:id">
+                <Detail/>
+              </Route>
+            </Switch>
+        </Router>
+  ;
 }
 
 export default App;
